685099c71ab369c1f1f1e1173d3e0b6cb84d930f
Ian Teoh <33985851+ianteohsc@users.noreply.github.com>
2023-11-23 15:58:53 +0800

    Update setup.py to v0.3

--- setup.py
+++ setup.py
@@ -1,25 +1,40 @@
 from distutils.core import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
 setup(
-  name = 'cachian',         # How you named your package folder (MyLib)
-  packages = ['cachian'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Python lru_cache but with TTL',   # Give a short description about your library
-  author = 'Ian Teoh',                   # Type in your name
-  author_email = 'ian.teoh@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/ianteohsc/cachian',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ianteohsc/cachian/archive/refs/tags/v0.2.tar.gz',    # I explain this later on
-  keywords = ['LRU', 'cache', 'ttl'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-      ],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: 3.11',
-  ],
+    name='cachian',         # How you named your package folder (MyLib)
+    packages=['cachian'],   # Chose the same as "name"
+    version='0.3',      # Start with a small number and increase it with every change you make
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    license='MIT',
+    # Give a short description about your library
+    description='Python lru_cache but with TTL',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    author='Ian Teoh',                   # Type in your name
+    author_email='ian.teoh@gmail.com',      # Type in your E-Mail
+    # Provide either the link to your github or to your website
+    url='https://github.com/ianteohsc/cachian',
+    # I explain this later on
+    download_url='https://github.com/ianteohsc/cachian/archive/refs/tags/v0.2.tar.gz',
+    # Keywords that define your package best
+    keywords=['LRU', 'cache', 'ttl'],
+    install_requires=[            # I get to this in a second
+    ],
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 5 - Production/Stable',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
 )
